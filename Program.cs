19a85bb062393251417dd93fd21500dbb0a41ef1
Ivan Kochurkin <kvanttt@gmail.com>
2019-06-11 18:01:04 +0300

    Initial repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObfuscationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
